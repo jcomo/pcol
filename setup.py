810517eca185b096f977998e55572a59d75c9ae0
Jonathan Como <jonathan.como@gmail.com>
2015-05-03 17:03:49 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pcol',
+    version = '0.1.0',
+    description = "A straightforward color rendering library for Python",
+    url = 'https://github.com/jcomo/pcol',
+    author = 'Jonathan Como',
+    author_email = 'jonathan.como@gmail.com',
+    packages = find_packages(exclude=['docs', 'tests']),
+    install_requires = [],
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords = 'console terminal color tree'
+)
