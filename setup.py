bc6ef4332360b46b4990893d4d7ad9348d16aeb1
Jonathan Como <jonathan.como@gmail.com>
2015-05-03 17:29:27 -0700

    Up version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pcol',
-    version = '0.1.0',
+    version = '0.1.1',
     description = "A straightforward color rendering library for Python",
     url = 'https://github.com/jcomo/pcol',
     author = 'Jonathan Como',
     author_email = 'jonathan.como@gmail.com',
+    py_modules = ['pcol'],
     packages = find_packages(exclude=['docs', 'tests']),
     install_requires = [],
     classifiers = [
